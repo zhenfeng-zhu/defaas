730b7dd00c806da1fe36608830cb275086605431
zhuzhenfeng.code <zhuzhenfeng.code@bytedance.com>
2022-10-17 19:21:52 +0800

    mvp
    Change-Id: Ia70be105622c1a981f08221ce7eebcc9b3f7cab1

---
+++ routes/api/sum.ts
@@ -0,0 +1,30 @@
+import { HandlerContext } from "$fresh/server.ts";
+
+export const handler = async (
+  _req: Request,
+  _ctx: HandlerContext,
+): Response => {
+  const prefix = "data:text/typescript,";
+  const s = `
+import axiod from "https://deno.land/x/axiod/mod.ts";
+
+
+const { data } = await axiod<{ delay: string }>(
+  "https://postman-echo.com/delay/2"
+);
+
+export function handler(a: int, b: int) {
+    console.log(data);
+    return a+b;
+}
+`;
+
+  console.log(s);
+
+  const { handler } = await import(`${prefix}${s}`);
+  console.log(handler);
+
+  console.log(handler(1, 1));
+
+  return Response.json({'a': handler(1, 2)});
+};
